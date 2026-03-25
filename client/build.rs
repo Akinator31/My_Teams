89c0ee8f291990d3dec5b0047a42d6ced70a0568
Akinator31 <pavel.de-wavrechin@epitech.eu>
2026-03-25 10:20:19 +0100

    refactor(build): add macos compatibility

--- client/build.rs
+++ client/build.rs
@@ -2,11 +2,21 @@ fn main() {
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let project_root = std::path::Path::new(&manifest_dir).parent().unwrap();
     let lib_dir = project_root.join("libs");
-    println!("cargo:rustc-link-search=native={}", lib_dir.display());
-    println!("cargo:rustc-link-lib=dylib=myteams");
-    println!("cargo:rerun-if-changed={}", lib_dir.join("libmyteams.so").display());
+
+
+    if cfg!(target_os = "macos") {
+        println!("cargo:rustc-link-search=native={}", lib_dir.display());
+        println!("cargo:rustc-link-lib=dylib=myteams_macos");
+        println!("cargo:rerun-if-changed={}", lib_dir.join("libmyteams_macos.so").display());
+        println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/libs");
+        println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/../../libs");
+        return;
+    }
 
     if cfg!(target_os = "linux") {
+        println!("cargo:rustc-link-search=native={}", lib_dir.display());
+        println!("cargo:rustc-link-lib=dylib=myteams_linux");
+        println!("cargo:rerun-if-changed={}", lib_dir.join("libmyteams_linux.so").display());
         println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/libs");
         println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/../../libs");
     }
