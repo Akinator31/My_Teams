3d640cc7ed9a28beb83ccbf3992629fb3c853404
Akinator31 <pavel.de-wavrechin@epitech.eu>
2026-03-25 09:58:04 +0100

    fix(build): rename lib folder

--- client/build.rs
+++ client/build.rs
@@ -1,7 +1,13 @@
 fn main() {
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let lib_dir = std::path::Path::new(&manifest_dir).parent().unwrap().join("lib");
+    let project_root = std::path::Path::new(&manifest_dir).parent().unwrap();
+    let lib_dir = project_root.join("libs");
     println!("cargo:rustc-link-search=native={}", lib_dir.display());
     println!("cargo:rustc-link-lib=dylib=myteams");
     println!("cargo:rerun-if-changed={}", lib_dir.join("libmyteams.so").display());
+
+    if cfg!(target_os = "linux") {
+        println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/libs");
+        println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/../../libs");
+    }
 }
