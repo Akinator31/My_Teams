c2e17fae2d62c9c5bda789ceaa669e1f6f102329
Kevin SALANIER <kevin.salanier@epitech.eu>
2026-03-31 20:56:31 +0200

    feat(client): refactor client function to accept TcpStream and improve command handling

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,7 +2,8 @@ mod commands;
 mod errors;
 
 use std::env::args;
-use std::io::BufRead;
+use std::io::{BufRead};
+use std::net::TcpStream;
 use std::process::exit;
 use crate::commands::commands::commands;
 
@@ -10,7 +11,8 @@ fn usage(binName: String) {
     println!("USAGE: {} <ip> <port>", binName);
 }
 
-fn client() {
+fn client(mut stream: TcpStream) {
+    stream.set_nonblocking(true);
     let stdin = std::io::stdin();
     let mut lines = stdin.lock().lines();
 
@@ -22,9 +24,10 @@ fn client() {
                     continue
                 }
                 let command = line.split(" ").next().unwrap_or("");
+                let command_trimmed = command.strip_prefix("/").unwrap();
 
-                if let Some(command) = commands().get(command) {
-                    command();
+                if let Some(command_trimmed) = commands().get(command_trimmed) {
+                    command_trimmed();
                 } else {
                     println!("Unknow command!");
                 }
@@ -36,7 +39,7 @@ fn client() {
     }
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = args().collect();
 
     // libs::ClientLog::client_event_logged_in("Holamos".to_string(), "Holamos".to_string());
@@ -44,4 +47,11 @@ fn main() {
         usage(args[0].clone());
         exit(84);
     }
+    let ip: String = args[1].clone();
+    let port: u16 = args[2].clone().parse().unwrap();
+    let address = format!("{}:{}", ip, port);
+
+    let mut stream = TcpStream::connect(address)?;
+    client(stream); // parcontre ça stop le serveur quand je stop le client wtf (Error: IoError)
+    Ok(())
 }
