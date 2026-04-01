bafc434ee4d6d40360fbf7fd5200758b9aae1acd
Akinator31 <pavel.de-wavrechin@epitech.eu>
2026-04-01 10:30:38 +0200

    fix(utils): add target config to generate uuid on linux and macos

--- server/src/utils/uuid.rs
+++ server/src/utils/uuid.rs
@@ -2,9 +2,22 @@ use std::fs::File;
 use std::io::Read;
 
 pub fn get_uuid() -> String {
-    let mut uuid = String::new();
-    let mut uuid_file = File::open("/proc/sys/kernel/random/uuid").unwrap();
+    #[cfg(target_os = "linux")]
+    {
+        let mut uuid = String::new();
+        let mut uuid_file = File::open("/proc/sys/kernel/random/uuid").unwrap();
+        uuid_file.read_to_string(&mut uuid).expect("/proc/sys/kernel/random/uuid doesn't exist!");
+        uuid.trim().to_string()
+    }
 
-    uuid_file.read_to_string(&mut uuid).expect("/proc/sys/kernel/random/uuid doesn't exist!");
-    uuid.trim().to_string()
+    #[cfg(target_os = "macos")]
+    {
+        let output = std::process::Command::new("uuidgen")
+            .output()
+            .expect("failed to run uuidgen");
+        String::from_utf8(output.stdout)
+            .expect("invalid utf8")
+            .trim()
+            .to_lowercase()
+    }
 }
