7836d3ad9edd32dbdc72ec882df5dc3277c351a1
Akinator31 <paveldewavrechin@gmail.com>
2026-04-05 17:19:44 +0200

    refactor(server): create many structs and functions to manipulate MyTeams data

--- server/src/server/data/channel.rs
+++ server/src/server/data/channel.rs
@@ -1,9 +1,40 @@
-use crate::server::data::team::Team;
 use crate::server::data::thread::Thread;
+use crate::utils::uuid::get_uuid;
 
 pub struct Channel {
-    pub team: Team,
+    pub team: String,
     pub uuid: String,
     pub name: String,
+    pub description: String,
     pub threads: Vec<Thread>,
+}
+
+pub struct ChannelCreatedEvent {
+    pub channel_uuid: String,
+    pub name: String,
+    pub description: String,
+    pub team_uuid: String,
+}
+
+impl Channel {
+    pub fn new(team_uuid: String, name: String, description: String) -> Self {
+        Channel {
+            team: team_uuid,
+            uuid: get_uuid(),
+            name,
+            description,
+            threads: Vec::new(),
+        }
+    }
+}
+
+impl From<(String, String, String, String)> for ChannelCreatedEvent {
+    fn from(value: (String, String, String, String)) -> Self {
+        ChannelCreatedEvent {
+            channel_uuid: value.0,
+            name: value.1,
+            description: value.2,
+            team_uuid: value.3,
+        }
+    }
 }
--- server/src/server/data/team.rs
+++ server/src/server/data/team.rs
@@ -1,9 +1,43 @@
 use crate::server::data::channel::Channel;
 use crate::server::data::user::User;
+use crate::utils::uuid::get_uuid;
 
 pub struct Team {
     pub uuid: String,
     pub name: String,
-    pub author: User,
+    pub author: String,
+    pub description: String,
     pub channels: Vec<Channel>,
+    pub subscribed: Vec<User>,
+}
+
+pub struct TeamCreatedEvent {
+    pub team_uuid: String,
+    pub name: String,
+    pub description: String,
+    pub creator_uuid: String,
+}
+
+impl Team {
+    pub fn new(name: String, description: String, user_uuid: String) -> Self {
+        Team {
+            uuid: get_uuid(),
+            name,
+            author: user_uuid,
+            description,
+            channels: Vec::new(),
+            subscribed: Vec::new(),
+        }
+    }
+}
+
+impl From<(String, String, String, String)> for TeamCreatedEvent {
+    fn from(value: (String, String, String, String)) -> Self {
+        TeamCreatedEvent {
+            team_uuid: value.0,
+            name: value.1,
+            description: value.2,
+            creator_uuid: value.3,
+        }
+    }
 }
