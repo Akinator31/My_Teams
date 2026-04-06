a0e101cc19890d15481a5f66b056f5ce66db5aff
Akinator31 <paveldewavrechin@gmail.com>
2026-04-06 13:41:55 +0200

    feat(server): add SUBSCRIBE command

---
+++ server/src/server/commands/subscribe.rs
@@ -0,0 +1,34 @@
+use crate::clients::client::ErrorCode::{NotFound, Unauthorized};
+use crate::clients::client::EventType;
+use crate::clients::client::OkeyResponse::SubscribedToTeam;
+use crate::clients::client::SuccessCode::Okay;
+use crate::server::commands::check_command_format;
+use crate::server::server::MyTeamsServer;
+
+pub fn subscribe(server: &mut MyTeamsServer, client_index: usize, command_args: String) -> bool {
+    let Some(user_uuid) = server.client_manager.clients[client_index].uuid.clone() else {
+        server.client_manager.clients[client_index].write(Unauthorized);
+        return true;
+    };
+
+    let Some(team_uuid): Option<String> = check_command_format(&command_args) else {
+        return false;
+    };
+
+    if !server
+        .data
+        .subscribe_to_team(team_uuid.clone(), user_uuid.clone())
+    {
+        server.client_manager.clients[client_index].write(NotFound);
+    } else {
+        server.client_manager.clients[client_index].write(Okay(SubscribedToTeam));
+
+        server.send_global_event(
+            EventType::UserSubscribed((user_uuid.clone(), team_uuid.clone()).into()),
+            Some(team_uuid.clone()),
+        )
+    }
+
+    libs::ServerLog::server_event_user_subscribed(team_uuid, user_uuid);
+    true
+}
