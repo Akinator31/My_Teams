1c2808ec8d854048a425bd2f40e2fbd0bbb494cd
Nogo4 <noah.gomes@epitech.eu>
2026-04-13 17:16:55 +0200

    fix(events): trim event string before splitting to prevent errors

--- client/src/events/events.rs
+++ client/src/events/events.rs
@@ -9,7 +9,7 @@ use std::collections::HashMap;
 type EventHandlerType = HashMap<String, fn(&str)>;
 
 pub fn events(event: &str) {
-    let mut parts = event.splitn(3, ' ');
+    let mut parts = event.trim().splitn(3, ' ');
     let _prefix = parts.next();
     let event_type = parts.next().unwrap_or("");
     let event_data = parts.next().unwrap_or("");
