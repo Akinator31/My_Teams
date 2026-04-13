e6fc42db3ecc47219342142f7e1ab27b0c04b174
Nogo4 <noah.gomes@epitech.eu>
2026-04-13 12:57:14 +0200

    feat(events): implement login event handling and update event module

--- client/src/events/events.rs
+++ client/src/events/events.rs
@@ -1,13 +1,21 @@
+use crate::events::login::login_event;
 use std::collections::HashMap;
 
 type EventHandlerType = HashMap<String, fn(&str)>;
 
-pub fn events(event: &str) -> EventHandlerType {
+pub fn events(event: &str) {
     let mut parts = event.splitn(3, ' ');
     let _prefix = parts.next();
     let event_type = parts.next().unwrap_or("");
     let event_data = parts.next().unwrap_or("");
+
     let mut handlers: EventHandlerType = HashMap::new();
 
-    handlers
+    handlers.insert("USER_LOGGED_IN".to_string(), login_event);
+
+    if let Some(handler) = handlers.get(event_type) {
+        handler(event_data);
+    } else {
+        println!("Unknown event type: {}", event_type);
+    }
 }
---
+++ client/src/events/login.rs
@@ -0,0 +1,12 @@
+use libs::ClientLog;
+
+pub fn login_event(data: &str) {
+    let uuid = data
+        .split(' ')
+        .nth(0)
+        .unwrap_or("")
+        .trim_matches('\"')
+        .to_string();
+    let username = data.split(' ').nth(1).unwrap_or("").to_string();
+    ClientLog::client_event_logged_in(uuid, username);
+}
