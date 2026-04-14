8b699dc758f09a38f6978ae3282e06f1689ef219
Kevin SALANIER <kevin.salanier@epitech.eu>
2026-04-14 17:39:20 +0200

    feat(client): enhance create command to log team, channel, and thread creation details

--- client/src/commands/create.rs
+++ client/src/commands/create.rs
@@ -1,4 +1,5 @@
-use crate::client::io_manager::IoManager;
+use libs::ClientLog;
+use crate::client::io_manager::{Context, IoManager};
 use crate::transport::{print_colored_reply, reply_code};
 
 pub fn create(io_manager: &mut IoManager, args: &str) {
@@ -15,16 +16,45 @@ pub fn create(io_manager: &mut IoManager, args: &str) {
             return;
         }
     };
-    match reply_code(&reply) {
-        Some(201) => {
+    match (reply_code(&reply), &io_manager.context) {
+        (Some(201), Context::None) => {
             print_colored_reply(&reply);
-            return;
+            let parts: Vec<&str> = reply.splitn(1, ' ').collect();
+            let args_parts: Vec<&str> = reply.splitn(2, ' ').collect();
+            let team_uuid = parts[0].trim_matches('"').to_string();
+            let team_name = args_parts[0].trim_matches('"').to_string();
+            let team_description = args_parts[1].trim_matches('"').to_string();
+            ClientLog::client_print_team_created(team_uuid, team_name, team_description);
         }
-        Some(_) => {
+        (Some(201), Context::Team(channel_uuid)) => {
             print_colored_reply(&reply);
+            let args_parts: Vec<&str> = reply.splitn(2, ' ').collect();
+            let channel_name = args_parts[0].trim_matches('"').to_string();
+            let channel_description = args_parts[1].trim_matches('"').to_string();
+            ClientLog::client_print_channel_created(channel_uuid.to_string(), channel_name, channel_description);
         }
-        None => {
-            println!("{}", reply.trim_end_matches(|c| c == '\r' || c == '\n'));
+        (Some(201), Context::Channel(_)) => {
+            print_colored_reply(&reply);
+            let parts: Vec<&str> = reply.splitn(2, ' ').collect();
+            let args_parts: Vec<&str> = reply.splitn(2, ' ').collect();
+            let thread_uuid = parts[0].trim_matches('"').to_string();
+            let thread_timestamp: i64 = parts[1].parse().unwrap_or(0);
+            let thread_title = args_parts[0].trim_matches('"').to_string();
+            let thread_description = args_parts[1].trim_matches('"').to_string();
+            ClientLog::client_print_thread_created(thread_uuid, io_manager.user_uuid.clone().unwrap().to_string(), thread_timestamp, thread_title, thread_description);
+        }
+        (Some(202), Context::Thread(ctx)) => {
+            print_colored_reply(&reply);
+            let parts: Vec<&str> = reply.splitn(2, ' ').collect();
+            let thread_uuid = parts[0].trim_matches('"').to_string();
+            let reply_timestamp: i64 = parts[1].parse().unwrap_or(0);
+            ClientLog::client_print_reply_created(thread_uuid, io_manager.user_uuid.clone().unwrap().to_string(), reply_timestamp, args.to_string());
+        }
+
+        // errors need to be implemented
+
+        _ => {
+
         }
     }
 }
