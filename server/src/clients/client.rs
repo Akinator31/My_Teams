91f23090306dfe4e3791abe9dac997627d369e75
Akinator31 <paveldewavrechin@gmail.com>
2026-04-17 18:53:02 +0200

    fix(server): add missing CLRF in UserNotFound server response

--- server/src/clients/client.rs
+++ server/src/clients/client.rs
@@ -229,7 +229,7 @@ impl From<ErrorCode> for String {
             ErrorCode::TeamNotFound(team_uuid) => format!("404 TEAM not found. \"{}\"\r\n", team_uuid),
             ErrorCode::ChannelNotFound(channel_uuid) => format!("404 CHANNEL not found. \"{}\"\r\n", channel_uuid),
             ErrorCode::ThreadNotFound(thread_uuid) => format!("404 THREAD not found. \"{}\"\r\n", thread_uuid),
-            ErrorCode::UserNotFound(user_uuid) => format!("404 USER not found. \"{}\"", user_uuid),
+            ErrorCode::UserNotFound(user_uuid) => format!("404 USER not found. \"{}\"\r\n", user_uuid),
             ErrorCode::Unauthorized => "401 Unauthorized\r\n".to_string(),
             ErrorCode::NoContextSet => "411 No context set\r\n".to_string(),
             ErrorCode::AlreadyExists => "409 Already exists\r\n".to_string(),
