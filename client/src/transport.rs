91d738b1fe6cf36847f478ac9424b3fe32a5fabc
Akinator31 <paveldewavrechin@gmail.com>
2026-04-17 19:33:47 +0200

    tests(server/client): add units tests

--- client/src/transport.rs
+++ client/src/transport.rs
@@ -3,6 +3,24 @@ pub fn reply_code(reply: &str) -> Option<u16> {
     code_str.parse::<u16>().ok()
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_reply_code_valid() {
+        assert_eq!(reply_code("200 OK\r\n"), Some(200));
+        assert_eq!(reply_code("404 Not Found"), Some(404));
+    }
+
+    #[test]
+    fn test_reply_code_invalid() {
+        assert_eq!(reply_code("EVENT USER_LOGGED_IN"), None);
+        assert_eq!(reply_code(""), None);
+        assert_eq!(reply_code("ABC Def"), None);
+    }
+}
+
 pub fn print_colored_reply(reply: &str) {
     let code = reply_code(reply);
 
